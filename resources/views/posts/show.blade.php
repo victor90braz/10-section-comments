e9bb89e39c96ec96b99c8fdc8d48da78df96c8a9
victor braz <victor.90braz@gmail.com>
2023-10-28 06:43:23 +0200

    create a post comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,24 +52,6 @@
     </section>
 
     <section class="col-span-8 col-start-5 mt-10">
-        <article class="flex bg-gray-100 p-4 border-gray-200 rounded-xl space-x-4">
-            <div style="flex-shrink: 0">
-                <img src="https://i.pravatar.cc/100" alt="avatar user" style="100px">
-            </div>
-            <div>
-                <header>
-                    <h3 class="font-bold">Braz</h3>
-                    <p class="text-xs">
-                        Posted <time>8 months ago</time>
-                    </p>
-                </header>
-
-                <main>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae ut nobis impedit quis quod dolore omnis sunt! Exercitationem nihil possimus porro quasi voluptatibus fuga fugit doloremque incidunt nesciunt dolorum.
-                    </p>
-                </main>
-            </div>
-        </article>
+        <x-post-comment />
     </section>
 </x-layout>
