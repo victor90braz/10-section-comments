6614ed4ff7b9b09ae4b2e6378393dd8f785d79c9
victor braz <victor.90braz@gmail.com>
2023-10-28 07:25:33 +0200

    $table->foreignId('post_id')->constrained()->cascadeOnDelete();

--- database/migrations/2023_10_28_044811_create_comments_table.php
+++ database/migrations/2023_10_28_044811_create_comments_table.php
@@ -15,14 +15,10 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->unsignedBigInteger('user_id');
             $table->text('body');
             $table->timestamps();
-            $table->foreign('post_id')
-                ->references('id')
-                ->on('posts')
-                ->onDelete('cascade');
         });
     }
 
