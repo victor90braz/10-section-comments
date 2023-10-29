53f91644377b71636aff143f21548d8083124bd8
victor braz <victor.90braz@gmail.com>
2023-10-29 18:09:16 +0100

    panel.blade

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -72,7 +72,7 @@
                     </textarea>
                 </main>
 
-                <div class="mt-2 flex justify-end pt-6">
+                <div class="mt-1 flex justify-end">
                     <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
                 </div>
             </form>
