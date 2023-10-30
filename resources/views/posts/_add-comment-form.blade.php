d39fff2e76a1d28eabedc0a86778ea706e842132
victor braz <victor.90braz@gmail.com>
2023-10-30 15:33:57 +0100

    submit buttton

--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -28,7 +28,7 @@
             </main>
 
             <div class="mt-1 flex justify-end">
-                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                <x-submit-button />
             </div>
         </form>
     </x-panel>
