05318a2162bfcff8482881ee0d9761d0e96118e9
victor braz <victor.90braz@gmail.com>
2023-10-30 15:30:24 +0100

    @include('posts/_add-comment-form')

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,42 +52,7 @@
     </section>
 
     <section>
-        @auth
-            <x-panel>
-                <form action="{{ $post->slug }}/comments" method="POST">
-                    @csrf
-
-                    <header class="flex justify-center items-center">
-                        @auth
-                            <img class="rounded-full" src="https://i.pravatar.cc/100?u={{ auth()->user()->id }}" alt="avatar user" style="width: 100px">
-                        @else
-                            <img class="rounded-full" src="https://i.pravatar.cc/100" alt="default avatar" width="100">
-                        @endauth
-
-                        <h2 class="ml-4">Want to participate?</h2>
-                    </header>
-
-                    <main class="mt-6">
-                        <textarea name="body"
-                                class="w-full text-sm focus:outline-none focus:ring border border border-gray-300"
-                                cols="30"
-                                rows="5"
-                                placeholder="Quick, think of something to say!"></textarea>
-                    </main>
-
-                    <div class="mt-1 flex justify-end">
-                        <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                    </div>
-                </form>
-            </x-panel>
-        @else
-            <x-panel class="flex justify-center mt-4">
-                    <p class="text-lg font-semibold">
-                        <a href="/register" class="text-blue-500 hover:underline">Register</a> or
-                        <a href="/login" class="text-blue-500 hover:underline">Login</a> to leave a comment.
-                    </p>
-            </x-panel>
-        @endauth
+        @include('posts/_add-comment-form')
     </section>
 
     <section class="col-span-8 col-start-5 mt-10 space-y-6">
